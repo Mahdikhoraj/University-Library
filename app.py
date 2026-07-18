b31222101d8c910d59adbc0dc40ea33fc71a08ef
Mahdi khoraj <mahdi87@gmail.com>
2026-07-18 18:57:04 +0330

    modified and add library class

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 from menus import*
+from library import*
 import time
 
 def main()->None:
     print("Welcome to the Iniversity library")
     time.sleep(1)
-    menu = Mainmenu()
-    menu.show_menu()
-    choice = menu.get_choice()
-    print(choice)
+    # menu = Mainmenu()
+    # menu.show_menu()
+    # choice = menu.get_choice()
+    # print(choice)
+    lib = Library()
+    lib.add_book("mmd", "nas", "1234")
+    lib.add_book("mmd", "nas", "1234")
+    lib.show_list_library()
+    lib.remove_book("mmd")
+    lib.add_book("ali", "nas", "-1234")
+    lib.show_list_library()
+    print(lib.library)
+    
     
 if __name__ == "__main__":
     main()
