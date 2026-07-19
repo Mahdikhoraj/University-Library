8a39d17dc7e3ce5f3c205b2252e54746f92cd9dc
Mahdi khoraj <mahdi87@gmail.com>
2026-07-19 09:42:14 +0330

    call the Book and library class with featuers

--- app.py
+++ app.py
@@ -3,21 +3,26 @@ from library import*
 import time
 
 def main()->None:
-    print("Welcome to the Iniversity library")
+    print("Welcome to the University library")
     time.sleep(1)
     # menu = Mainmenu()
     # menu.show_menu()
     # choice = menu.get_choice()
     # print(choice)
     lib = Library()
-    lib.add_book("mmd", "nas", "1234")
-    lib.add_book("mmd", "nas", "1234")
-    lib.show_list_library()
-    lib.remove_book("mmd")
-    lib.add_book("ali", "nas", "-1234")
-    lib.show_list_library()
-    print(lib.library)
+    lib.add_book("Clean Code", "Robert C. Martin", "850")
+    lib.add_book("Python Crash Course", "Eric Matthes", "1200")
+    lib.add_book("The Pragmatic Programmer", "Andrew Hunt", "1500")
+    lib.remove_book("Clean Code")
+    lib.add_book("ali", "nas", "1234")
+    lib.update_price("ali","1387")
+    lib.show_books()
     
+    book1 = lib.find_book("The Pragmatic Programmer")
+    book2  = lib.find_book("Python Crash Course")
+    print(book1 == book2)
+    print(int(book1))
+    print(len(book1))
     
 if __name__ == "__main__":
     main()
