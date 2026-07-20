4940692c8e445c487cb63079a148aec6093bc4e7
Mahdi khoraj <mahdi87@gmail.com>
2026-07-20 18:30:41 +0330

    modified and edit Book class

--- book.py
+++ book.py
@@ -1,24 +1,40 @@
-class Book():
-    def __init__(self,book_title,auther,price):
-        self.title = book_title
-        self.auther  = auther
-        self.price = int(price)
-        
+__all__ = [
+    "Book",
+]
+class Book:
+    def __init__(self, title, author, price):
+        self.title = title
+        self.author = author
+        self.price = price
+
     def show_info(self):
-        print(f"Book : {self.title}")
-        print(f"auther : {self.auther}")
-        print(f"price : {self.price}")
-        
-        
-    def change_price(self,price):
-        self.price = int(price)
-        
-        
+
+        return f"\nBook:{self.title}\n Author:{self.author}\n Price:{self.price}\n {"-" * 60}"
+
+    def discount(self, percent):
+        result = self.price * (percent / 100)
+        self.price -= result
+
+    def __str__(self):
+        return self.show_info()
+
+    def __repr__(self):
+        return f"Book('{self.title}' , '{self.author}' , '{self.price}')"
+
+    def __eq__(self, other):
+        if self.title == other.title and self.author == other.author:
+            return True
+        return False
+
+    def __lt__(self, other):
+        if self.price < other.price:
+            return True
+        return False
+
+    def __gt__(self, other):
+        if self.price > other.price:
+            return True
+        return False
+
     def __int__(self):
         return self.price
-    
-    def __len__(self):
-        return len( self.title)
-    
-    def __eq__(self, other):
-        return self.price  == other.price
