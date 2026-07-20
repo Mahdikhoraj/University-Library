c9e8e4d0264d688c4e4bfa59fb12c3721b567d39
Mahdi khoraj <mahdi87@gmail.com>
2026-07-20 18:31:29 +0330

    modified and edit library class in library.py

--- library.py
+++ library.py
@@ -1,57 +1,25 @@
 from book import Book
+from valid import Validations
+
 __all__ = [
     "Library",
 ]
+
+
 class Library:
-
     def __init__(self):
-        self.library = []
+        self.books = list()
 
-    def add_book(self, title, auther, price):
-        if int(price) < 0:
-            print("Price cannot be negative.")
-            return
-        book = Book(title,auther,price)
-        self.library.append(book)
-        print("Book added")
-    def remove_book(self, title):
-            for book in self.library:
-                if book.title == title:
-                    self.library.remove(book)
-                    print("Book removed.")
-                    return
-
-            print("Book not found.")
-
-    def show_books(self):
-        print("-" * 20)
-        for book in self.library:
-            book.show_info()
-            print("-" * 20)
-            
-            
-    def update_price(self,title,new_price):
-        for book in self.library:
-            if book.title == title:
-                book.change_price(new_price)
-                return
+    def add_book(self, book):
+        if Validations.is_valid_price(book.price):
+            self.books.append(book)
         else:
-            print("book not found")
-            
-    def find_book(self,title):
-        for book in self.library:
-            if book.title ==title:
-                return book
-        return None
-        
+            print("The price cant be negetive or etc")
 
-# lib = Library()
+    def remove_book(self, book):
+        if self.is_valid_price(book):
+            self.books.remove(book)
 
-# lib.add_book("mmd", "nas", "1234")
-# lib.add_book("mmd", "nas", "1234")
-# lib.add_book("mmd", "nas", "1234")
-# lib.remove_book("mmd")
-
-# lib.show_books()
-
-
+    def show_list_book(self):
+        for book in self.books:
+            print(book)
